9448128685678dd4850f8af400f4c855c56076fa
ozturk sirin <ozturk2002.zri@gmail.com>
2024-02-26 13:47:37 +0300

    first

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "databasePassword",
+        database: "blogapp"
+    }
+}
+
+module.exports = config;
